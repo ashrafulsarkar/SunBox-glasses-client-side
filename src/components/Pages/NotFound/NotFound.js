b80201a1f4bcd071e192407e151f0afb53a9fe9f
ashrafulsarkar <ashrafulsarkar47@gmail.com>
2021-11-13 02:14:32 +0600

    not found page add

--- src/components/Pages/NotFound/NotFound.js
+++ src/components/Pages/NotFound/NotFound.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            
+        <div className="section-p not-found">
+            <div className="container">
+                <div className="row">
+                    <div className="col text-center">
+                        <h3 className="text-center">404</h3>
+                        <h3 className="text-center">Not Found</h3>
+                        <Link to="/">Go to Home</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
